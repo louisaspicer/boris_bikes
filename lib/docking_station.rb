395732c8036267a479821d8be9ca4c1a9d7a3330
Louisa <louisaspicer@gmail.com>
2016-11-08 18:20:02 +0000

    tried test to return bike object

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,7 +1,9 @@
+require './lib/bike.rb'
+
 class DockingStation
 
   def release_bike
-    
+    @bike = Bike.new
   end
 
 end
--- spec/docking_object_spec.rb
+++ spec/docking_object_spec.rb
@@ -1,5 +1,12 @@
 require './lib/docking_station'
 
 describe DockingStation do
+
   it { is_expected.to respond_to :release_bike }
-  end
+
+end
+
+describe 'release_bike' do
+  it "return bike" do
+    expect(DockingStation.new.release_bike).to be_a Bike
+end
