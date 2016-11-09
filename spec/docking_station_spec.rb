27b297c92fc549796a13da1b54c5db202c458f26
Louisa <louisaspicer@gmail.com>
2016-11-09 15:10:29 +0000

    Added tests to DockingStation specto get a bike and expects it to be working

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -2,4 +2,8 @@ require 'docking_station_class'
 
 describe DockingStation do
   it { is_expected.to respond_to :release_bike }
+
+  it { expect(DockingStation.new.release_bike).to be_an_instance_of(Bike) }
+
+  it { expect(DockingStation.new.release_bike.working?).to eq true }
 end
