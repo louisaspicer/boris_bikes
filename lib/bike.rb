c8d0cd6ee94e1ca13d42cf953367971e1eb802d0
Jenna Ramdenee <jenna_ramdenee@hotmail.co.uk>
2016-11-10 16:14:50 +0000

    ex18 in progress

--- lib/bike.rb
+++ lib/bike.rb
@@ -1,9 +1,19 @@
 require './lib/docking_station_class.rb'
 
 class Bike
+  attr_accessor :broken
+
+  def initialize(working = true)
+    @working = working
+  end
 
   def working?
-   true
+   @working
   end
+
+  def report_as_broken
+    @working = false
+  end
+
 
 end
--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -2,4 +2,16 @@ require 'bike'
 
 describe Bike do
   it {is_expected.to respond_to :working?}
+  it {is_expected.to respond_to :report_as_broken}
+
+  it 'marks a bike as broken' do
+    subject.report_as_broken
+    expect(subject.working?).to eq false
+  end
+
+  it 'identifies if a bike is not broken' do
+    expect(subject.working?).to eq true
+  end
+
+
 end
