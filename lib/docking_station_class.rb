74db99e0a8847ae03dad19c76b95da228e1468bd
Louisa <louisaspicer@gmail.com>
2016-11-10 10:55:15 +0000

    tested for error and used guard condition

--- lib/docking_station_class.rb
+++ lib/docking_station_class.rb
@@ -5,7 +5,17 @@ class DockingStation
   attr_reader :bike
 
   def release_bike
-    @bike
+    raise(RuntimeError, "No bikes available") if check_for_bikes == false
+    generate_bike
+
+  end
+
+  def generate_bike
+    Bike.new
+  end
+
+  def check_for_bikes
+    false
   end
 
   def dock(bike)
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -4,9 +4,9 @@ describe DockingStation do
 
   it { is_expected.to respond_to :release_bike }
 
-  it { expect(DockingStation.new.release_bike).to be_an_instance_of(Bike) }
+  it { expect(subject.generate_bike).to be_an_instance_of(Bike) }
 
-  it { expect(DockingStation.new.release_bike.working?).to eq true }
+  it { expect(subject.generate_bike.working?).to eq true }
 
   it { is_expected.to respond_to :dock }
 
@@ -23,16 +23,8 @@ describe DockingStation do
     expect(subject.bike).to eq bike
   end
 
-  describe '#release_bike' do
-    it 'releases a bike' do
-      bike = Bike.new
-      subject.dock(bike)
-      expect(subject.release_bike).to eq bike
-    end
-  end
-
   it 'raises error if no bikes' do
-    expect { DockingStation.new.release_bike }.to raise_error(RuntimeError, "No bikes available")
+    expect { subject.release_bike }.to raise_error(RuntimeError, "No bikes available")
   end
 
 end
